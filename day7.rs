dd60716f9b95f9c37b7189cde72b8b7e211e51ed
tmokenc <tmokenc@protonmail.com>
2021-12-09 03:01:19 +0100

    update

--- day7.rs
+++ day7.rs
@@ -1,52 +1,53 @@
-fn the_treachery_of_whales(input: &[i32]) -> u64 {
-    if input.is_empty() {
-        return 0;
-    }
-    
-    let mut min = i32::MAX;
-    let mut max = i32::MIN;
-    
-    for i in input {
-        if *i < min {
-            min = *i;
-        } else if *i > max {
-            max = *i;
+mod part1 {
+    pub fn the_treachery_of_whales(input: &[i32]) -> u64 {
+        if input.is_empty() {
+            return 0;
         }
+
+        let mut min = i32::MAX;
+        let mut max = i32::MIN;
+
+        for i in input {
+            if *i < min {
+                min = *i;
+            } else if *i > max {
+                max = *i;
+            }
+        }
+
+        (min..=max)
+            .map(|v| input.iter().copied().map(|x| (v - x).abs()).sum())
+            .min()
+            .unwrap_or(0i32) as u64
     }
-    
-    (min..=max)
-        .map(|v| {
-            input.iter().copied().map(|x| (v - x).abs()).sum()
-        })
-        .min()
-        .unwrap_or(0i32) as u64
 }
 
-fn the_treachery_of_whales_part2(input: &[i32]) -> u64 {
-    if input.is_empty() {
-        return 0;
-    }
-    
-    let mut min = i32::MAX;
-    let mut max = i32::MIN;
-    
-    for i in input {
-        if *i < min {
-            min = *i;
-        } else if *i > max {
-            max = *i;
+mod part2 {
+    pub fn the_treachery_of_whales(input: &[i32]) -> u64 {
+        if input.is_empty() {
+            return 0;
         }
-    }
-    
-    (min..=max)
-        .map(|v| {
-            input.iter().copied().map(|x| {
+
+        let mut min = i32::MAX;
+        let mut max = i32::MIN;
+
+        for i in input {
+            if *i < min {
+                min = *i;
+            } else if *i > max {
+                max = *i;
+            }
+        }
+
+        (min..=max)
+            .map(|v| input.iter().copied().map(|x| {
                 let steps = (v - x).abs();
                 (0..=steps).sum::<i32>()
-            }).sum()
-        })
-        .min()
-        .unwrap_or(0i32) as u64
+            })
+            .sum())
+            .min()
+            .unwrap_or(0i32) as u64
+    }
 }
 
 fn main() -> std::io::Result<()> {
@@ -54,17 +55,17 @@ fn main() -> std::io::Result<()> {
         .split(",")
         .filter_map(|v| v.parse::<i32>().ok())
         .collect::<Vec<_>>();
-    
+
     let example_input = std::fs::read_to_string("./input/example/day7.txt")?
         .split(",")
         .filter_map(|v| v.parse::<i32>().ok())
         .collect::<Vec<_>>();
-    
-    println!("Day7 Part 1 Example: {}", the_treachery_of_whales(&example_input));
-    println!("Day7 Part 1: {}", the_treachery_of_whales(&input));
-    
-    println!("Day7 Part 2 Example: {}", the_treachery_of_whales_part2(&example_input));
-    println!("Day7 Part 2: {}", the_treachery_of_whales_part2(&input));
-    
+
+    println!("Day7 Part 1 Example: {}", part1::the_treachery_of_whales(&example_input));
+    println!("Day7 Part 1: {}", part1::the_treachery_of_whales(&input));
+
+    println!("Day7 Part 2 Example: {}", part2::the_treachery_of_whales(&example_input));
+    println!("Day7 Part 2: {}", part2::the_treachery_of_whales(&input));
+
     Ok(())
-} 
+}
--- day8.rs
+++ day8.rs
@@ -1,133 +1,134 @@
-use std::convert::TryInto;
+mod part1 {
+    pub fn seven_segment_search(input: &str) -> u64 {
+        let mut count = 0;
 
-fn has_char_in_str(ch: char, s: &str) -> bool {
-    s.chars().any(|v| v == ch)
-}
+        for line in input.lines() {
+            let mut split = line.split('|').map(|v| v.trim());
+            let _wired = split.next().unwrap();
+            let display = split.next().unwrap();
 
-fn get_off_chars(s: &str, full: &str) -> Vec<char> {
-    full.chars()
-        .filter(|v| !has_char_in_str(*v, s))
-        .collect()
-}
-    
-fn parse_digits<'a>(s: &'a str) -> [&'a str; 10] {
-    let mut digits = [""; 10];
-    let mut unknown = Vec::new();
-    
-    for digit in s.split_whitespace() {
-        let known_by_length = match digit.len() {
-            2 => Some(1),
-            3 => Some(7),
-            4 => Some(4),
-            7 => Some(8),
-            _ => {
-                unknown.push(digit);
-                None
+            for digit in display.split_whitespace() {
+                if matches!(digit.len(), 2 | 3 | 4 | 7) {
+                    count += 1;
+                }
             }
-        };
-        
-        if let Some(idx) = known_by_length {
-            digits[idx] = digit;
         }
+
+        count
     }
-    
-    while let Some(digit) = unknown.pop() {
-        if digit.len() == 6 {
-            let off = get_off_chars(digit, digits[8])[0];
-            
-            if has_char_in_str(off, digits[1]) {
-                digits[6] = digit;
-                continue
+}
+
+mod part2 {
+    use std::convert::TryInto;
+
+    fn has_char_in_str(ch: char, s: &str) -> bool {
+        s.chars().any(|v| v == ch)
+    }
+
+    fn get_off_chars(s: &str, full: &str) -> Vec<char> {
+        full.chars().filter(|v| !has_char_in_str(*v, s)).collect()
+    }
+
+    fn parse_digits<'a>(s: &'a str) -> [&'a str; 10] {
+        let mut digits = [""; 10];
+        let mut unknown = Vec::new();
+
+        for digit in s.split_whitespace() {
+            let known_by_length = match digit.len() {
+                2 => Some(1),
+                3 => Some(7),
+                4 => Some(4),
+                7 => Some(8),
+                _ => {
+                    unknown.push(digit);
+                    None
+                }
+            };
+
+            if let Some(idx) = known_by_length {
+                digits[idx] = digit;
             }
-            
-            if has_char_in_str(off, digits[4]) {
-                digits[0] = digit;
+        }
+
+        while let Some(digit) = unknown.pop() {
+            if digit.len() == 6 {
+                let off = get_off_chars(digit, digits[8])[0];
+
+                if has_char_in_str(off, digits[1]) {
+                    digits[6] = digit;
+                    continue;
+                }
+
+                if has_char_in_str(off, digits[4]) {
+                    digits[0] = digit;
+                    continue;
+                }
+
+                digits[9] = digit;
                 continue;
             }
-            
-            digits[9] = digit;
-            continue
-        }
-        
-        let [a, b]: [char; 2] = get_off_chars(digit, digits[8]).try_into().unwrap();
-        
-        if !has_char_in_str(a, digits[1]) && !has_char_in_str(b, digits[1]) {
-            digits[3] = digit;
-            continue
-        }
-        
-        if has_char_in_str(a, digits[4]) && has_char_in_str(b, digits[4]) {
-            digits[2] = digit;
-        } else {
-            digits[5] = digit;
-        }
-    }
-    
-    digits
-}
 
-fn get_digit(digit: &str, digits: &[&str]) -> u8 {
-    for i in 0..10 {
-        if digit.len() != digits[i].len() {
-            continue;
-        }
-        
-        if digit.chars().all(|v| has_char_in_str(v, digits[i])) {
-            return i as u8;
-        }
-    }
-    
-    0
-}
+            let [a, b]: [char; 2] = get_off_chars(digit, digits[8]).try_into().unwrap();
 
-fn seven_segment_search(input: &str) -> u64 {
-    let mut count = 0;
-    
-    for line in input.lines() {
-        let mut split = line.split('|').map(|v| v.trim());
-        let _wired = split.next().unwrap();
-        let display = split.next().unwrap();
-        
-        for digit in display.split_whitespace() {
-            if matches!(digit.len(), 2 | 3 | 4 | 7) {
-                count += 1;
+            if !has_char_in_str(a, digits[1]) && !has_char_in_str(b, digits[1]) {
+                digits[3] = digit;
+                continue;
+            }
+
+            if has_char_in_str(a, digits[4]) && has_char_in_str(b, digits[4]) {
+                digits[2] = digit;
+            } else {
+                digits[5] = digit;
             }
         }
-    }
-    
-    count
-}
 
-fn seven_segment_search_part_2(input: &str) -> u64 {
-    let mut total = 0;
-    
-    for line in input.lines() {
-        let mut split = line.split('|').map(|v| v.trim());
-        let wired = split.next().unwrap();
-        let display = split.next().unwrap();
-        let digits = parse_digits(wired);
-        let mut res = 0;
-        
-        for digit in display.split_whitespace() {
-            res *= 10;
-            res += get_digit(digit, &digits) as u64;
+        digits
+    }
+
+    fn get_digit(digit: &str, digits: &[&str]) -> u8 {
+        for i in 0..10 {
+            if digit.len() != digits[i].len() {
+                continue;
+            }
+
+            if digit.chars().all(|v| has_char_in_str(v, digits[i])) {
+                return i as u8;
+            }
         }
-        
-        total += res;
-    }
-    
-    total
-}
 
+        0
+    }
+
+    pub fn seven_segment_search(input: &str) -> u64 {
+        let mut total = 0;
+
+        for line in input.lines() {
+            let mut split = line.split('|').map(|v| v.trim());
+            let wired = split.next().unwrap();
+            let display = split.next().unwrap();
+            let digits = parse_digits(wired);
+            let mut res = 0;
+
+            for digit in display.split_whitespace() {
+                res *= 10;
+                res += get_digit(digit, &digits) as u64;
+            }
+
+            total += res;
+        }
+
+        total
+    }
+}
 fn main() -> std::io::Result<()> {
     let input = std::fs::read_to_string("./input/day8.txt")?;
     let example_input = std::fs::read_to_string("./input/example/day8.txt")?;
-    
-    println!("Day8 Part 1 Example: {}", seven_segment_search(&example_input));
-    println!("Day8 Part 1: {}", seven_segment_search(&input));
-    
-    println!("Day8 Part 2 Example: {}", seven_segment_search_part_2(&example_input));
-    println!("Day8 Part 2: {}", seven_segment_search_part_2(&input));
-    
+
+    println!("Day8 Part 1 Example: {}", part1::seven_segment_search(&example_input));
+    println!("Day8 Part 1: {}", part1::seven_segment_search(&input));
+
+    println!("Day8 Part 2 Example: {}", part2::seven_segment_search(&example_input));
+    println!("Day8 Part 2: {}", part2::seven_segment_search(&input));
+
     Ok(())
 }
