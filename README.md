f2dc06eb50d482a205c5aa901540c0a02e0b8930
tmokenc <tmokenc@protonmail.com>
2021-12-17 07:09:17 +0900

    coloring

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Advent-of-Code-2021
-My solution for the advent of code 2021, mainly written in Rust
+My solution for the advent of code 2021, written in Rust
+
+# Running time
+My implementation solves AoC 2021 problems from microsecond (us) to 5 milliseconds (ms)<br>
+Exception is the problem day 12 (Passage Pathing) - 88ms
+
+# Error Handle
+**NOPE!!!** `unwrap()` everything everywhere
+
+# Use
+To run all of the advent of code solutions
+```sh
+cargo run --release
+```
+To run only a specific day (replace {DAY_NUMBER} with the number of that day)
+```sh
+cargo run --release -- {DAY_NUMBER}
+```
+
+# Dependencies
+**humantime** - to display the running time
+**owo-colors** - to print colored result in terminal
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ mod day11;
 mod day12;
 mod day13;
 mod day14;
+mod day15;
 
 const NAMES: &[&str] = &[
     "Sonar Sweep",
@@ -33,6 +34,7 @@ const NAMES: &[&str] = &[
 ];
 
 use humantime::format_duration;
+use owo_colors::OwoColorize as _;
 use std::path::Path;
 use std::time::{Duration, Instant};
 
@@ -71,13 +73,23 @@ impl Solution {
 
 impl Solution {
     fn run(&self) {
-        println!("Input parsed in {}", format_duration(self.parse_time));
+        println!("Input parsed in {}", format_duration(self.parse_time).bright_magenta());
 
         let (res, timed) = time(|| self.app.part1());
-        println!("Part 1: {} ({})", res, format_duration(timed));
+        println!(
+            "Part 1: {} ({}) (total {})", 
+            res, 
+            format_duration(timed).cyan(),
+            format_duration(timed + self.parse_time).bright_cyan(),
+        );
 
         let (res, timed) = time(|| self.app.part2());
-        println!("Part 2: {} ({})", res, format_duration(timed));
+        println!(
+            "Part 2: {} ({}) (total {})", 
+            res, 
+            format_duration(timed).cyan(),
+            format_duration(timed + self.parse_time).bright_cyan(),
+        );
 
         println!("");
     }
@@ -110,26 +122,28 @@ impl Solutions {
 
     fn get(day: u8) -> Option<Self> {
         match day {
-            01 => Self::new::<day01::SonarSweep>(1),
-            02 => Self::new::<day02::Dive>(2),
-            03 => Self::new::<day03::BinaryDiagnostic>(3),
-            04 => Self::new::<day04::GiantSquid>(4),
-            05 => Self::new::<day05::HydrothermalVenture>(5),
-            06 => Self::new::<day06::Lanternfish>(6),
-            07 => Self::new::<day07::TheTreacheryOfWhales>(7),
-            08 => Self::new::<day08::SevenSegmentSearch>(8),
-            09 => Self::new::<day09::SmokeBasin>(9),
-            10 => Self::new::<day10::SyntaxScoring>(10),
-            11 => Self::new::<day11::DumboOctopus>(11),
-            12 => Self::new::<day12::PassagePathing>(12),
-            13 => Self::new::<day13::TransparentOrigami>(13),
-            14 => Self::new::<day14::ExtendedPolymerization>(14),
+            01 => Self::new::<day01::SonarSweep>(day),
+            02 => Self::new::<day02::Dive>(day),
+            03 => Self::new::<day03::BinaryDiagnostic>(day),
+            04 => Self::new::<day04::GiantSquid>(day),
+            05 => Self::new::<day05::HydrothermalVenture>(day),
+            06 => Self::new::<day06::Lanternfish>(day),
+            07 => Self::new::<day07::TheTreacheryOfWhales>(day),
+            08 => Self::new::<day08::SevenSegmentSearch>(day),
+            09 => Self::new::<day09::SmokeBasin>(day),
+            10 => Self::new::<day10::SyntaxScoring>(day),
+            11 => Self::new::<day11::DumboOctopus>(day),
+            12 => Self::new::<day12::PassagePathing>(day),
+            13 => Self::new::<day13::TransparentOrigami>(day),
+            14 => Self::new::<day14::ExtendedPolymerization>(day),
+            15 => Self::new::<day15::Chiton>(day),
             _ => None,
         }
     }
 
     fn run(&self) {
-        println!("DAY {} - {}", self.day, NAMES[self.day as usize - 1]);
+        let title = format!("DAY {} - {}", self.day, NAMES[self.day as usize - 1]);
+        println!("{}", title.bold());
 
         println!("Example");
         self.example.run();
@@ -139,7 +153,7 @@ impl Solutions {
     }
 }
 
-fn main() -> Result<(), String> {
+fn main() {
     let mut days = std::env::args()
         .skip(1)
         .filter_map(|v| v.parse::<u8>().ok())
@@ -150,5 +164,4 @@ fn main() -> Result<(), String> {
     }
     
     days.into_iter().filter_map(|d| Solutions::get(d)).for_each(|v| v.run());
-    Ok(())
 }
